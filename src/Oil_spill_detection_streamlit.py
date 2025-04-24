ece52153fba0535ea69bde15f93b99fa80f5f2aa
Yash Juare <yashjuare3@gmail.com>
2025-04-24 18:39:17 +0530

    'app'

--- src/Oil_spill_detection_streamlit.py
+++ src/Oil_spill_detection_streamlit.py
@@ -5,16 +5,13 @@ import cv2
 from tensorflow.keras.models import model_from_json
 from PIL import Image
 import os
-
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-
 import keras
 
-json_file = open("Oil_Spilled_Detection/model210425.json", 'r')
+json_file = open(r"C:\Users\HP\oil_spills_detection\src\model210425.json", 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("Oil_Spilled_Detection/model210425.weights.h5")
+loaded_model.load_weights(r"C:\Users\HP\oil_spills_detection\src\model210425.weights.h5")
 
 def predict(data):
     resized_image = cv2.resize(data, (128, 128))
@@ -24,8 +21,17 @@ def predict(data):
     return predictions
 
 def main():
-    st.title("Oil Spilled Detection in Sea Water ")
-    st.sidebar.title("Oil Spilled Detection in Sea Water")
+    page_bg_img = '''
+    <style>
+    stMainBlockContainer block-container st-emotion-cache-yw8pof ekr3hml4 {
+    background-color: bluevoilet;
+    }
+    </style>
+    '''
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    st.title("Oil Spill Detection System", )
+    st.sidebar.title("Oil Spill Detection System ")
     html_temp = """
         <div style="background-color:tomato;padding:10px">
         <h2 style="color:white;text-align:center;">Convolutional_neural_network </h2>
@@ -36,18 +42,35 @@ def main():
     st.markdown(html_temp, unsafe_allow_html = True)
     st.text("")
     
-
+    
     file_upload = st.sidebar.file_uploader("Choose SAR image file of sea water " ,type=['.jpg','.png'],accept_multiple_files=False,key="fileUploader")
     if file_upload is not None :
             data = Image.open(file_upload)
             
             data = np.array(data)
         
-            st.image(data, caption="Uploaded Image.", use_column_width= True)  
+            st.image(data, caption="Uploaded Image.", use_container_width= True)  
         
     else:
         st.sidebar.warning("you need to upload image file.")
 
+    option = st.sidebar.selectbox(
+    "Sample images of oil spilled ocean",
+    ("Class1_sample1.jpg", "Class1_sample2.jpg", "Class1_sample3.jpg", 'Class1_sample4.jfif', 'Class1_sample5.jfif', 'Class1_sample6.jfif'), index= None, placeholder= 'select samples',)
+    if option is not None:
+        data = Image.open(option)
+        data = np.array(data)
+        st.image(data, caption="Uploaded Image.", use_column_width= True)
+        st.sidebar.write("You selected:", option)
+
+    option = st.sidebar.selectbox(
+    "Sample images of clean ocean",
+    ("Class0_sample1.jpg", "Class0_sample2.jpg", "Class0_sample3.jpg", 'Class0_sample4.jfif', 'Class0_sample5.jfif', 'Class0_sample6.jfif'), index= None, placeholder= 'select samples',)
+    if option is not None:
+        data = Image.open(option)
+        data = np.array(data)
+        st.image(data, caption="Uploaded Image.", use_column_width= True)
+        st.sidebar.write("You selected:", option)
             
     result = ""
     if st.button("Predict"):
