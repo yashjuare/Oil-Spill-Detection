fd4b3a5da64359332aba13a5971a6981807fdb0e
yashjuare <105511092+yashjuare@users.noreply.github.com>
2025-06-19 00:46:23 +0530

    Update Oil_spill_detection_streamlit.py

--- src/Oil_spill_detection_streamlit.py
+++ src/Oil_spill_detection_streamlit.py
@@ -30,7 +30,7 @@ def main():
         """
         <style>
         .stApp {
-            background-image: url("https://images.unsplash.com/photo-1618502396341-5c680e7d7233?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+            background-image: url("https://static.vecteezy.com/system/resources/previews/027/804/562/non_2x/blue-water-surface-background-free-photo.jpg");
             background-size: cover;
         }
         </style>
